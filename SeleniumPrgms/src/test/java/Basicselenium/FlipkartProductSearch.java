bf306069c0a08c07955ee36789e5d260f859a17e
kalyanesree <mshoba778@gmail.com>
2025-12-10 13:55:11 +0530

    corrected code

--- SeleniumPrgms/src/test/java/Basicselenium/FlipkartProductSearch.java
+++ SeleniumPrgms/src/test/java/Basicselenium/FlipkartProductSearch.java
@@ -1,78 +1,41 @@
 package Basicselenium;
 
-
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class FlipkartProductSearch {
 
     public static void main(String[] args) throws Exception {
 
        
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    	WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		System.out.println("------------------------");
+		//for laptop
+		driver.findElement(By.name("q")).sendKeys("Laptop");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		WebElement laptop7 = driver.findElement(By.xpath("(//a[@class='k7wcnx'])[7]"));
+		System.out.println("Laptop no.7: " + laptop7.getText());
+		System.out.println("------------------------");
+		//for tv
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.name("q")).sendKeys("TV");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		WebElement tv13 = driver.findElement(By.xpath("(//a[@class='k7wcnx'])[13]"));
+		System.out.println("TV no.13: " + tv13.getText());
+		System.out.println("------------------------");
+		//for phone
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.name("q")).sendKeys("Smart Phone");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		WebElement phone2 = driver.findElement(By.xpath("(//a[@class='k7wcnx'])[2]"));
+		System.out.println("Smart Phone no.2: " + phone2.getText());
+		System.out.println("------------------------");
+		driver.quit();
 
-        driver.get("https://www.flipkart.com");
-
-        Thread.sleep(3000);
-
-     // Close login popup using ESC key
-        try {
-            Thread.sleep(3000);
-            driver.findElement(By.tagName("body")).sendKeys(Keys.ESCAPE);
-            System.out.println("Login popup closed using ESC key.");
-        } catch (Exception e) {
-            System.out.println("Popup not shown.");
-        }
-
-
-        // Laptop - 7th product
-        searchAndPrint(driver, "Laptop", 7);
-
-        // TV - 13th product
-        searchAndPrint(driver, "TV", 13);
-
-        // Smart Phone - 2nd product
-        searchAndPrint(driver, "Smart Phone", 2);
-
-        driver.quit();
-    }
-
-    static void searchAndPrint(WebDriver driver, String product, int n) throws Exception {
-
-        Thread.sleep(3000);
-
-        // ✅ Re-locate the search box freshly every time
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.click();
-
-        // ✅ FORCE CLEAR using CTRL + A + BACKSPACE
-        searchBox.sendKeys(Keys.CONTROL + "a");
-        searchBox.sendKeys(Keys.BACK_SPACE);
-
-        Thread.sleep(1000);
-
-        // ✅ Type new product name
-        searchBox.sendKeys(product);
-        searchBox.sendKeys(Keys.ENTER);
-
-        Thread.sleep(5000);
-
-        List<WebElement> products = driver.findElements(By.cssSelector("div._4rR01T"));
-
-        System.out.println("Product searched: " + product);
-
-        if (products.size() >= n) {
-            System.out.println("Nth product: " + products.get(n - 1).getText());
-        } else {
-            System.out.println("Less number of products found.");
-        }
-
-        System.out.println("----------------------------------------");
     }
 }
